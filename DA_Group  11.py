83ba8329fa804c4cdd5f2ca7f7984c92d6b9f3c7
Hareeshhh <89488631+Hareeshhh@users.noreply.github.com>
2021-08-29 23:39:45 +0800

    Update DA_Group  11.py

--- DA_Group  11.py
+++ DA_Group  11.py
@@ -1,5 +1,27 @@
 # Use the Request library
 import requests
+import scrapy
+
+class NewSpider(scrapy.Spider):
+        name = "new_spider"
+        start_urls = ['https://brickset.com/sets/year-2008']
+        def parse(self, response):
+                xpath_selector='//img'
+                for x in response.xpath(xpath_selector):
+                    newsel='@src'
+                    yield{
+                            'image Link': x.xpath(newsel).extract_first(),
+                    }
+
+
+         # To recurse next page
+                    Page_selector='.next a ::attr(href)'
+                    next_page=response.css(Page_selector).extract_first()
+                    if next_page:
+                                yield scrapy.Request(
+                                        response.urljoin(next_page),
+                                        callback=self.parse
+                                )
 # Set the target webpage
 url = 'https://brickset.com/sets/year-2008'
 r = requests.get(url)
