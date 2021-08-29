7317a566ff16be5e6394c2c3676813bbef79725e
Hareeshhh <89488631+Hareeshhh@users.noreply.github.com>
2021-08-29 18:45:16 +0800

    Update DA_Group  11.py

--- DA_Group  11.py
+++ DA_Group  11.py
@@ -1,7 +1,1 @@
-# Use the Reuqest library
-import requests
-# Set the target webpage
-url = 'https://brickset.com/sets/year-2008'
-r = requests.get(url)
-# This will get the full page
-print(r.text)
+
